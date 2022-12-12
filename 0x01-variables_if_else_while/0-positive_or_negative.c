5f7a200f98952b198b175a9b38fb86131b9d45f5
mante <yawmante8994@gmail.com>
2022-12-12 12:26:51 -0800

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,6 @@ int main(void)
 {
 	int n;
 
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n<0)
@@ -23,5 +22,4 @@ int main(void)
 	else
 		printf("%d is positive\n", n);
 	return (0);
-
 }
